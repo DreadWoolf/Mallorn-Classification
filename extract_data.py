963e42aff0c19f9b23bceeb7445d8b1995f3a408
DreadWoolf <rasmus.eliasson@live.se>
2025-12-17 14:10:07 +0100

    data_pre_processing progress

--- extract_data.py
+++ extract_data.py
@@ -5,29 +5,73 @@ import pandas as pd
 import os
 
 
+main_folder = "mallorn-astronomical-classification-challenge"
+data_folder = "Data"
 
-def merge_splits():
-    main_folder = "mallorn-astronomical-classification-challenge"
-    data_folder = "Data"
-    path = os.path.join(os.getcwd(), main_folder)
-    data_path = os.path.join(os.getcwd(), data_folder)
+path = os.path.join(os.getcwd(), main_folder)
+data_path = os.path.join(os.getcwd(), data_folder)
+
+
+
+def merge_splits(train_or_test = 'train'):
     # Path to your main folder
 
     # Get all unique directories
     data_paths_list = [os.path.join(main_folder, d) for d in os.listdir(main_folder) if os.path.isdir(os.path.join(main_folder, d))]
 
-    data_splits = pd.DataFrame()
+    df = pd.DataFrame()
 
     dfs = []
 
-    for split in data_paths_list:
-        df = pd.read_csv(
-            os.path.join(split, "train_full_lightcurves.csv"),
+    for split_path in data_paths_list:
+        split = pd.read_csv(
+            os.path.join(split_path, f"{train_or_test.lower()}_full_lightcurves.csv"),
             sep=","
         )
-        dfs.append(df)
+
+        
+
+        dfs.append(build_per_filter_features(split, "?"))
+        # dfs.append(split)
 
     df_splits = pd.concat(dfs, ignore_index=True)
 
     return df_splits
 
+
+
+
+def merge_and_save_data():
+
+    df_dict = {
+
+        "train": pd.DataFrame(),
+        "test": pd.DataFrame()
+    }
+    
+
+    for part in df_dict.keys(): #["train", "test"]:
+        df = pd.read_csv(os.path.join(main_folder, f"{part}_log.csv"), sep=",")
+
+        features = merge_splits(part)
+
+        df = pd.read_csv(
+            os.path.join(main_folder, f"{part}_full_lightcurves.csv"),
+            sep=","
+        )
+
+        df_dict[part] = features.merge(
+            df,
+            on=["object_id", "split_id"],  # or just object_id if unique
+            how="left"
+        )
+    
+        df_dict[part].to_csv(os.path.join(data_folder, f"MALLORN-data_{part}.csv"), index=False)
+
+    
+    return df_dict["train"], df_dict["test"]
+
+
+
+def build_per_filter_features(df, string):
+    pass
